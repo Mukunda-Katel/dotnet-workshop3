f8b1912626db13e808d867b1ca84020ecccfdba0
Mukunda-Katel <mukundakatel@gmail.com>
2025-11-12 12:22:52 +0545

    Completed task 6

---
+++ task6/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Task_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            try
+            {
+                int number = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("You entered: " + number);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid number format.");
+            }
+            finally
+            {
+                Console.WriteLine("Program Executed.");
+            }
+
+            Console.WriteLine();
+
+            Console.Write("Enter a password: ");
+            string password = Console.ReadLine();
+
+            try
+            {
+                if (password.Length < 6)
+                {
+                    throw new Exception("Password must be at least 6 characters.");
+                }
+
+                Console.WriteLine("Password satisfied the requirement.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
