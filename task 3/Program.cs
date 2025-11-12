e0984968d33c5d30802562708465a60592003ef9
Mukunda-Katel <mukundakatel@gmail.com>
2025-11-12 11:46:01 +0545

    Completed task3

--- task 3/Program.cs
+++ task 3/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter your age below");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(age);
+
+            if (age < 13)
+            { 
+                    Console.WriteLine("Child");
+            }
+
+            if (age >=13 & age <=19)
+            {
+                Console.WriteLine("Teenager");
+            }
+
+            if (age >= 20)
+            {
+                Console.WriteLine("Adult");
+
+            }
+
+
         }
     }
 }
